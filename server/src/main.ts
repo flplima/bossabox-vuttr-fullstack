bb1abcd4ea028b6e5daafcf2f4d0291bd05aa732
flplima <felipe.lds@live.com>
2020-08-29 17:37:35 -0300

    chore: Update HTTP port

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-NestFactory.create(AppModule).then(app => app.listen(3001));
+NestFactory.create(AppModule).then(app => {
+  app.listen(process.env.HTTP_PORT);
+});
