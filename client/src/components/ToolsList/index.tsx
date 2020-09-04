2ca9e0489fca6be8e92c177174b3afaf32991b62
flplima <felipe.lds@live.com>
2020-09-04 18:14:34 -0300

    styles: Smooth transitions in list

--- client/src/components/ToolsList/index.tsx
+++ client/src/components/ToolsList/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import useSWR from "swr";
 import { useSetRecoilState, useRecoilState } from "recoil";
 import Highlighter from "react-highlight-words";
+import { motion, AnimatePresence } from "framer-motion";
 
 import {
   ToolItem,
@@ -51,16 +52,31 @@ const ToolsList: React.FC = () => {
     setSearchQuery(tag);
   };
 
-  if (!data) return <div>Loading...</div>;
-
-  if (!data.length) return <ToolsPlaceholder>No tools found.</ToolsPlaceholder>;
-
   return (
-    <>
-      {data.map((tool) => (
-        <ToolItem key={tool.id}>
-          {tool.link ? (
-            <a href={tool.link} target="_blank" rel="noopener noreferrer">
+    <AnimatePresence exitBeforeEnter>
+      <motion.div
+        key={data?.length}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.1 }}
+      >
+        {data?.length === 0 && (
+          <ToolsPlaceholder>No tools found.</ToolsPlaceholder>
+        )}
+        {data?.map((tool) => (
+          <ToolItem key={tool.id}>
+            {tool.link ? (
+              <a href={tool.link} target="_blank" rel="noopener noreferrer">
+                <ToolTitle>
+                  <Highlighter
+                    searchWords={searchTagsOnly ? [] : [searchQuery]}
+                    autoEscape={true}
+                    textToHighlight={tool.title}
+                  />
+                </ToolTitle>
+              </a>
+            ) : (
               <ToolTitle>
                 <Highlighter
                   searchWords={searchTagsOnly ? [] : [searchQuery]}
@@ -68,42 +84,34 @@ const ToolsList: React.FC = () => {
                   textToHighlight={tool.title}
                 />
               </ToolTitle>
-            </a>
-          ) : (
-            <ToolTitle>
+            )}
+            <ButtonRemove onClick={onClickRemoveTool(tool)}>
+              <FontAwesomeIcon icon={faTimes} />
+              <span>remove</span>
+            </ButtonRemove>
+            <ToolDescription>
               <Highlighter
                 searchWords={searchTagsOnly ? [] : [searchQuery]}
                 autoEscape={true}
-                textToHighlight={tool.title}
+                textToHighlight={tool.description}
               />
-            </ToolTitle>
-          )}
-          <ButtonRemove onClick={onClickRemoveTool(tool)}>
-            <FontAwesomeIcon icon={faTimes} />
-            <span>remove</span>
-          </ButtonRemove>
-          <ToolDescription>
-            <Highlighter
-              searchWords={searchTagsOnly ? [] : [searchQuery]}
-              autoEscape={true}
-              textToHighlight={tool.description}
-            />
-          </ToolDescription>
-          <span>
-            {tool.tags.map((tag) => (
-              <TagLink onClick={onClickTag(tag)}>
-                <Highlighter
-                  key={tag}
-                  searchWords={[searchQuery]}
-                  autoEscape={true}
-                  textToHighlight={`#${tag}`}
-                />
-              </TagLink>
-            ))}
-          </span>
-        </ToolItem>
-      ))}
-    </>
+            </ToolDescription>
+            <span>
+              {tool.tags.map((tag) => (
+                <TagLink onClick={onClickTag(tag)}>
+                  <Highlighter
+                    key={tag}
+                    searchWords={[searchQuery]}
+                    autoEscape={true}
+                    textToHighlight={`#${tag}`}
+                  />
+                </TagLink>
+              ))}
+            </span>
+          </ToolItem>
+        ))}
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
